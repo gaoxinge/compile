2181b74f2383dfe8e0c4fc656e37f3c0047ab82d
gaoxinge <gaoxx5@gmail.com>
2016-08-13 15:19:18 +0800

    compile

--- compiler/compiler.c
+++ compiler/compiler.c
@@ -8,6 +8,19 @@ char *src, *old_src;
 int poolsize;
 int line;
 
+int *text, *old_text, *stack;
+char data;
+int *pc, *bp, *sp, ax, cycle;
+
+enum{LEA, IMM, JMP, CALL, JZ, JNZ, ENT, ADJ, LEV, LI, LC, SI, SC, PUSH, 
+	 OR, XOR, AND, EQ, NE, LT, GT, LE, GE, SHL, SHR, ADD, SUB, MUL, DIV, MOD,
+	 OPEN, READ, CLOS, PRTF, MALC, MSET, MCMP, EXIT};
+
+int eval()
+{
+	return 0;
+}
+	 
 void next()
 {
 	token = *src++;
@@ -27,11 +40,6 @@ void program()
 void expression(int level)
 {
 	
-}
-
-int eval()
-{
-	return 0;
 }
 
 int main(int argc, char **argv)
@@ -64,6 +72,42 @@ int main(int argc, char **argv)
 	
 	src[i] = 0;
 	close(fd);
+	
+	if(!(text = old_text = malloc(poolsize)))
+	{
+		printf("could not malloc(%d) for text area\n", poolsize);
+		return -1;
+	}
+	
+	if(!(data = malloc(poolsize)))
+	{
+		printf("could not malloc(%d) for data area\n", poolsize);
+		return -1;
+	}
+	
+	if(!(stack = malloc(poolsize)))
+	{
+		printf("could not malloc(%d) for stack area\n", poolsize);
+		return -1;
+	}
+	
+	memset(text, 0, poolsize);
+	memset(data, 0, poolsize);
+	memset(stack, 0, poolsize);
+	
+	bp = sp =(int *)((int) stack + poolsize);
+	ax = 0;
+	i = 0;
+	text[i++] = IMM;
+	text[i++] = 10;
+	text[i++] = PUSH;
+	text[i++] = IMM;
+	text[i++] = 20;
+	text[i++] = ADD;
+	text[i++] = PUSH;
+	text[i++] = EXIT;
+	pc = text;
+	
 	program();
 	return eval();
 }
