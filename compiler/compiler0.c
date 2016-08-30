4cfaa73980ea4b6ec4e4c483d2c821c2d0de718c
gaoxinge <gaoxx5@gmail.com>
2016-08-30 08:27:13 +0800

    compiler

--- compiler/compiler0.c
+++ compiler/compiler0.c
@@ -208,6 +208,59 @@ void match(int tk)
 }
 
 //PARSER
+int eval()
+{
+	int op, *tmp;
+	while(1)
+	{
+		op = *op++;
+		if(op == IMM)      {ax = *pc++;}
+		else if(op == LC)  {ax = *(char *)ax;}
+		else if(op == LI)  {ax = *(int *)ax;}
+		else if(op == SC)  {ax = *(char *)*sp++ = ax;}
+		else if(op == SI)  {*--sp = ax;}
+		else if(op == PUSH){*--sp = ax;}
+		else if(op == JMP) {pc = (int *)*pc;}
+		else if(op == JZ)  {pc = ax ? pc + 1 : (int *)* pc;}
+		else if(op == JNZ) {px = ax ? (int *)*pc : pc + 1;}
+		else if(op == CALL){*--sp = (int)(pc + 1); pc = (int *)*pc;}
+		else if(op == ENT) {*--sp = (int)bp; bp = sp; sp = sp - *pc++;}
+		else if(op == ADJ) {sp = sp + *pc++;}
+		else if(op == LEV) {sp = bp; bp = (int *)*sp++; pc = (int *)*sp++;}
+		else if(op == LEA) {ax = (int)(bp + *pc++);}
+		else if(op == OR)  {ax = *sp++ | ax;}
+		else if(op == XOR) {ax = *sp++ ^ ax;}
+		else if(op == AND) {ax = *sp++ & ax;}
+		else if(op == EQ)  {ax = *sp++ == ax;}
+		else if(op == NE)  {ax = *sp++ != ax;}
+		else if(op == LT)  {ax = *sp++ < ax;}
+		else if(op == LE)  {ax = *sp++ <= ax;}
+		else if(op == GT)  {ax = *sp++ > ax;}
+		else if(op == GE)  {ax = *sp++ >= ax;}
+		else if(op == SHL) {ax = *sp++ << ax;}
+		else if(op == SHR) {ax = *sp++ >> ax;}
+		else if(op == ADD) {ax = *sp++ + ax;}
+		else if(op == SUB) {ax = *sp++ - ax;}
+		else if(op == MUL) {ax = *sp++ * ax;}
+		else if(op == DIV) {ax = *sp++ / ax;}
+		else if(op == MOD) {ax = *sp++ % ax;}
+		else if(op == EXIT){printf("exit(%d)", *sp); return *sp;}
+		else if(op == OPEN){ax = open((char *)sp[1], sp[0]);}
+		else if(op == CLOS){ax = close(*sp);}
+		else if(op == READ){ax = read(sp[2], (char *)sp[1], *sp);}
+		else if(op == PRTF){tmp = sp + pc[1]; ax = printf((char *)tmp[-1], tmp[-2], tmp[-3], tmp[-4], tmp[-5], tmp[-6]);}
+		else if(op == MALC){ax = (int)malloc(*sp);}
+		else if(op == MSET){ax = (int)memset((char *)sp[2], sp[1], *sp);}
+		else if(op == MCMP){ax = memcmp((char *)sp[2], (char *)sp[1], *sp)}
+		else
+		{
+			printf("unknown instruction:%d\n", op);
+			return -1;
+		}	
+	}
+	return 0;
+}
+
 void program()
 {
 	next();
@@ -405,65 +458,50 @@ void function_body()
 		basetype = (token == Int) ? INT : CHAR;
 		match(token);
 		
+		while(token != ';')
+		{
+			type = basetype;
+			while(token == Mul)
+			{
+				match(Mul);
+				type = type + PTR;
+			}
+			
+			if(token != Id)
+			{
+				printf("%d: duplicate local declaration\n", line);
+				exit(-1);
+			}
+			
+			if(current_id[Class] == Loc)
+			{
+				printf("%d: duplicate local declaration\n", line);
+				exit(-1);
+			}
+			
+			match(Id);
+			current_id[BClass] = current_id[Class];
+			current_id[Class]  = Loc;
+			current_id[BType]  = current_id[Type];
+			current_id[Type]   = type;
+			current_id[BValue] = current_id[Value];
+			current_id[Value]  = ++pos_local;
+			
+			if(token == ','){match(',');}
+		}
+		match(';');
 	}
+	
+	*++text = ENT;
+	*++text = pos_local - index_of_bp;
+	while(token != '}'){statement();}
+	*++text = LEV;
 }
 
+//INTERPRETER
 void expression(int level)
 {
 	
-}
-
-int eval()
-{
-	int op, *tmp;
-	while(1)
-	{
-		op = *op++;
-		if(op == IMM)      {ax = *pc++;}
-		else if(op == LC)  {ax = *(char *)ax;}
-		else if(op == LI)  {ax = *(int *)ax;}
-		else if(op == SC)  {ax = *(char *)*sp++ = ax;}
-		else if(op == SI)  {*--sp = ax;}
-		else if(op == PUSH){*--sp = ax;}
-		else if(op == JMP) {pc = (int *)*pc;}
-		else if(op == JZ)  {pc = ax ? pc + 1 : (int *)* pc;}
-		else if(op == JNZ) {px = ax ? (int *)*pc : pc + 1;}
-		else if(op == CALL){*--sp = (int)(pc + 1); pc = (int *)*pc;}
-		else if(op == ENT) {*--sp = (int)bp; bp = sp; sp = sp - *pc++;}
-		else if(op == ADJ) {sp = sp + *pc++;}
-		else if(op == LEV) {sp = bp; bp = (int *)*sp++; pc = (int *)*sp++;}
-		else if(op == LEA) {ax = (int)(bp + *pc++);}
-		else if(op == OR)  {ax = *sp++ | ax;}
-		else if(op == XOR) {ax = *sp++ ^ ax;}
-		else if(op == AND) {ax = *sp++ & ax;}
-		else if(op == EQ)  {ax = *sp++ == ax;}
-		else if(op == NE)  {ax = *sp++ != ax;}
-		else if(op == LT)  {ax = *sp++ < ax;}
-		else if(op == LE)  {ax = *sp++ <= ax;}
-		else if(op == GT)  {ax = *sp++ > ax;}
-		else if(op == GE)  {ax = *sp++ >= ax;}
-		else if(op == SHL) {ax = *sp++ << ax;}
-		else if(op == SHR) {ax = *sp++ >> ax;}
-		else if(op == ADD) {ax = *sp++ + ax;}
-		else if(op == SUB) {ax = *sp++ - ax;}
-		else if(op == MUL) {ax = *sp++ * ax;}
-		else if(op == DIV) {ax = *sp++ / ax;}
-		else if(op == MOD) {ax = *sp++ % ax;}
-		else if(op == EXIT){printf("exit(%d)", *sp); return *sp;}
-		else if(op == OPEN){ax = open((char *)sp[1], sp[0]);}
-		else if(op == CLOS){ax = close(*sp);}
-		else if(op == READ){ax = read(sp[2], (char *)sp[1], *sp);}
-		else if(op == PRTF){tmp = sp + pc[1]; ax = printf((char *)tmp[-1], tmp[-2], tmp[-3], tmp[-4], tmp[-5], tmp[-6]);}
-		else if(op == MALC){ax = (int)malloc(*sp);}
-		else if(op == MSET){ax = (int)memset((char *)sp[2], sp[1], *sp);}
-		else if(op == MCMP){ax = memcmp((char *)sp[2], (char *)sp[1], *sp)}
-		else
-		{
-			printf("unknown instruction:%d\n", op);
-			return -1;
-		}	
-	}
-	return 0;
 }
 
 int main(int argc, char **argv)
