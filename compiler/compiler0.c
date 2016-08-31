4cf3269400371eebfadd5c10f97f802b45bf1c44
gaoxinge <gaoxx5@gmail.com>
2016-08-31 16:53:23 +0800

    compiler

--- compiler/compiler0.c
+++ compiler/compiler0.c
@@ -498,12 +498,81 @@ void function_body()
 	*++text = LEV;
 }
 
-//INTERPRETER
+void statement()
+{
+	int *a, *b;
+	
+	if(token == If)
+	{
+		match(If);
+		match('(');
+		expression(Assign);
+		match(')')
+		*++text = JZ;
+		b = ++text;
+		
+		statment();
+		if(token == Else)
+		{
+			match(Else);
+			*b = (int)(text + 3);
+			*++text = JMP;
+			b = ++text;
+			statement();
+		}
+		
+		*b = (int)(text + 1);
+	}
+	else if(token == While)
+	{
+		match(While);
+		a = text + 1;
+		match('(');
+		expression(Assign);
+		match(')');
+		*++text = JZ;
+		b = ++text;
+		statement();
+		*++text = JMP;
+		*++text = (int)a;
+		*b = (int)(text + 1);
+	}
+	else if(token == '{')
+	{
+		match('{');
+		while(token != '}')
+		{
+			statement();
+		}
+		match('}');
+	}
+	else if(token == Return)
+	{
+		match(Return);
+		if(token != ';')
+		{
+			expression(Assign);
+		}
+		match(';');
+		*++text = LEV;
+	}
+	else if(token == ';')
+	{
+		match(';');
+	}
+	else
+	{
+		expression(Assign);
+		match(';');
+	}
+}
+
 void expression(int level)
 {
 	
 }
 
+//INTERPRETER
 int main(int argc, char **argv)
 {
 	int i, fd;
